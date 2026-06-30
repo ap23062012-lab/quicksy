65c66b1ca7e7c1c4eb1bc368626a97375f7cc335
Ajay Pande <ap23062012@gmail.com>
2026-06-30 22:11:09 +0530

    Fix Next.js config for dynamic routes

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,8 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",   // 🔥 required for S3
-
   images: {
-    unoptimized: true, // required for static export
+    unoptimized: true,
   },
 
   turbopack: {
